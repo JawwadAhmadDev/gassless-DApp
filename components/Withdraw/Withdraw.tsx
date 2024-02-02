1a805f9e92c46ae1bda783656c48d425ad0dc426
Jawwad Ahmad <109460336+JawwadAhmadDev@users.noreply.github.com>
2024-02-02 18:12:47 +0500

    signature created successfully for withdraw

--- components/Withdraw/Withdraw.tsx
+++ components/Withdraw/Withdraw.tsx
@@ -1,47 +1,132 @@
 'use client'
-// GaslessWithdraw.tsx
 import React, { useState } from 'react';
 
-const GaslessWithdraw: React.FC = () => {
-    const [amount, setAmount] = useState<number>(0);
-    const [isSignatureCreated, setIsSignatureCreated] = useState<boolean>(false);
+import { useAccount } from 'wagmi';
+import { type ReadContractReturnType, getAccount, readContract, writeContract } from '@wagmi/core'
+import { config } from '@/config/wagmi';
+import toast from 'react-hot-toast';
+import { contract, uToken_ABI } from '@/contracts/uTokenFactory/contract';
+import { parseEther } from 'viem';
+import { createWithdrawSignature } from '@/utils/createWithdrawSig';
 
-    const handleCreateSignature = () => {
-        // Logic to create signature
-        setIsSignatureCreated(true);
+export type CreateWithdrawSigData = {
+    domainName: string,
+    chainId: number | undefined,
+    contractAddress: string,
+    relayerAddress: string,
+    amount: number,
+    message: string
+}
+
+type WithdrawData = {
+    uTokenAddress: string,
+    signer: string,
+    amount: number,
+    message: string,
+    signature: string
+}
+
+const GaslessWithdraw: React.FC = () => {
+    const [tokensAmount, setTokensAmount] = useState<number>(0);
+    const [isSignatureCreated, setIsSignatureCreated] = useState<boolean>(false);
+    const [relayerAddress, setRelayerAddress] = useState<string>('');
+    const [ERC20PermitAddress, setERC20PermitAddress] = useState<string>('');
+    const [withdrawData, setWithdrawData] = useState<WithdrawData>({
+        uTokenAddress: '',
+        signer: '',
+        amount: 0,
+        message: '',
+        signature: ''
+    })
+
+    const account = useAccount();
+    const currentAccount = getAccount(config);
+    
+    const handleCreateSignature = async () => {
+        const message = `I am authorizing relayer via MetaMask to perform "withdraw" transaction on my behalf, ensuring secure and efficient operations.`
+        try {
+
+            const uTokenAddress: ReadContractReturnType = await readContract(config, {
+                abi: uToken_ABI,
+                address: account.chainId === 5 ? contract.goerli : contract.mumbai,
+                functionName: 'get_uTokenAddressOfToken',
+                args: [ERC20PermitAddress], 
+            });
+
+        // collecting data for creation of signature
+            const createWithdrawSigData: CreateWithdrawSigData = {
+                domainName: "uTokenFactory",
+                chainId: account.chainId,
+                contractAddress: account.chainId === 5 ? contract.goerli : contract.mumbai,
+                relayerAddress: relayerAddress,
+                amount: Number(parseEther(String(tokensAmount))),
+                message: message
+            };
+
+            const signature = await createWithdrawSignature(createWithdrawSigData);
+            
+            setWithdrawData({...withdrawData, 
+                uTokenAddress: uTokenAddress,
+                signer: currentAccount.address as string,
+                amount: Number(parseEther(String(tokensAmount))),
+                message: message,
+                signature: signature
+            });
+            
+            setIsSignatureCreated(true);
+        } catch (error) {
+            toast.error(`Error while creating signature.\n ${error}`);
+        }
     };
 
     const handleWithdraw = () => {
         // Logic for withdrawal
-        console.log(`Withdrawing ${amount} tokens`);
+        console.log(withdrawData);
     };
 
     return (
         <div className="flex flex-col items-center justify-center p-5">
             <h1 className="text-2xl font-bold text-gray-200 mb-4">Gasless Withdraw</h1>
             
+           
+            <input 
+                type="text"
+                value={ERC20PermitAddress}
+                onChange={(e) => setERC20PermitAddress(e.target.value)}
+                className="mb-4 px-3 py-2 border border-gray-300 text-slate-900 rounded-md w-full"
+                placeholder="Token Address"
+            />
+
+            <input 
+                type="text"
+                value={relayerAddress}
+                onChange={(e) => setRelayerAddress(e.target.value)}
+                className="mb-4 px-3 py-2 border border-gray-300 text-slate-900 rounded-md w-full"
+                placeholder="Relayer Address"
+            />
+
             <input 
                 type="number"
-                value={amount}
-                onChange={(e) => setAmount(Number(e.target.value))}
+                value={tokensAmount}
+                onChange={(e) => setTokensAmount(Number(e.target.value))}
                 className="mb-4 px-3 py-2 border border-gray-300 text-slate-900 rounded-md w-full"
                 placeholder="Amount to withdraw"
             />
             
             <div className="flex gap-4">
                 <button 
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
                     onClick={handleCreateSignature}
-                    disabled={isSignatureCreated}
+                    disabled={isSignatureCreated || !(account.status === "connected")}
                 >
                     Create Signature
                 </button>
                 <button 
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
                     onClick={handleWithdraw}
-                    disabled={!isSignatureCreated}
+                    disabled={!isSignatureCreated || !(account.status === "connected")}
                 >
-                    Withdraw
+                    Deposit
                 </button>
             </div>
         </div>
